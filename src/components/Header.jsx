005288f1e57676cac5a2ba872b7416d567a8bf92
Alok Singh <aloksingh36757@gmail.com>
2025-07-24 05:37:24 +0530

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
    <header className='p-[15px] shadow-xl'>
     <div className='max-w-[1200px] mx-auto border border-red-500'>
         <div>
-<img src='images/Swiggy-emblem.png' alt=''/>
+
 
         </div>
 
