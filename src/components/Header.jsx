ca0deb94d4be457a40e1b9d77d94599ceb50b0d6
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-25 05:03:53 +0530

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
 import React from 'react'
+import { RxCaretDown } from "react-icons/rx";
 
 export default function Header() {
   return (
    <header className='p-[15px] shadow-xl'>
-    <div className='max-w-[1200px] mx-auto border border-red-500'>
-        <div className='w-[100px] border border-blue-500'>
+    <div className='max-w-[1200px] mx-auto border border-red-500 flex items-center'>
+        <div className='w-[100px]'>
 <img src="images/Swiggy-emblem.png" className='w-full' alt=''/>
  
         </div>
 
 
-        <div>
+        <div className=''>
+<span className='font-bold border-b-[3px] border-black'>Ratanada</span>
+ Jodhpur, Rajsthan, India <RxCaretDown fontsize={25} className='font-bold inline text-[.9rem] text-[#fc8019]'/>
 
-                
         </div>
 
 
