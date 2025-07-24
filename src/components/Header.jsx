a7abeadd6268e2cfbcc63fffd861ce08d012747d
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-25 05:12:33 +0530

    navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,30 @@
-import React from 'react'
+import React, { useState } from "react";
 import { RxCaretDown } from "react-icons/rx";
 
 export default function Header() {
+  const [toggle, setToggle] = useState(false);
+
   return (
-   <header className='p-[15px] shadow-xl'>
-    <div className='max-w-[1200px] mx-auto border border-red-500 flex items-center'>
-        <div className='w-[100px]'>
-<img src="images/Swiggy-emblem.png" className='w-full' alt=''/>
- 
+    <>
+    <div className='black-overlay'></div>
+      <header className="p-[15px] shadow-xl">
+        <div className="max-w-[1200px] mx-auto border border-red-500 flex items-center">
+          <div className="w-[100px]">
+            <img src="images/Swiggy-emblem.png" className="w-full" alt="" />
+          </div>
+
+          <div className="">
+            <span className="font-bold border-b-[3px] border-black">
+              Ratanada
+            </span>
+            Jodhpur, Rajsthan, India{" "}
+            <RxCaretDown
+              fontsize={25}
+              className="font-bold inline text-[.9rem] text-[#fc8019]"
+            />
+          </div>
         </div>
-
-
-        <div className=''>
-<span className='font-bold border-b-[3px] border-black'>Ratanada</span>
- Jodhpur, Rajsthan, India <RxCaretDown fontsize={25} className='font-bold inline text-[.9rem] text-[#fc8019]'/>
-
-        </div>
-
-
-
-
-    </div>
-
-
-   </header>
-  )
+      </header>
+    </>
+  );
 }
