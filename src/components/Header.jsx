f751d9a8b796aee91a6697c6ee26b24dfbb93fe6
Alok Singh <aloksingh36757@gmail.com>
2025-07-24 05:35:57 +0530

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 export default function Header() {
   return (
-   <header className='p-3 shadow-xl'>
+   <header className='p-[15px] shadow-xl'>
+    <div className='max-w-[1200px] mx-auto border border-red-500'>
+        <div>
+<img src='images/Swiggy-emblem.png' alt=''/>
+
+        </div>
+
+Hello 
+
+
+    </div>
 
 
    </header>
