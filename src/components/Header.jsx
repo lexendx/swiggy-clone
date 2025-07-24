c054452c6d41abadeed166f43c7fa3089d5bc29a
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-25 05:26:43 +0530

    Navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,10 +3,18 @@ import { RxCaretDown } from "react-icons/rx";
 
 export default function Header() {
   const [toggle, setToggle] = useState(false);
+  const showSideMenu = () => {
+setToggle(true);        
+
+  }
 
   return (
     <>
-    <div className='black-overlay'></div>
+    <div className='black-overlay w-full h-full fixed dur-500' style={{opacity: toggle ? 1 : 0,
+visibility: toggle ? "visible" : "hidden"
+
+
+    }}></div>
       <header className="p-[15px] shadow-xl">
         <div className="max-w-[1200px] mx-auto border border-red-500 flex items-center">
           <div className="w-[100px]">
@@ -17,11 +25,12 @@ export default function Header() {
             <span className="font-bold border-b-[3px] border-black">
               Ratanada
             </span>
-            Jodhpur, Rajsthan, India{" "}
+            Jodhpur, Rajsthan, India
             <RxCaretDown
-              fontsize={25}
-              className="font-bold inline text-[.9rem] text-[#fc8019]"
-            />
+  onClick={showSideMenu}
+  fontsize={25}
+  className="font-bold inline text-[.9rem] text-[#fc8019] cursor-pointer"
+/>
           </div>
         </div>
       </header>
