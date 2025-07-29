b7a5b560a42dde0135b8cc71a19d8457bc00819c
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-30 04:23:16 +0530

    minor changes

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -3,11 +3,11 @@ import React from 'react'
 export default function () {
   return (
     <div className='max-w-[1200px] mx-auto flex items-center'>
-    <div>
+    <div className='flex items-center justify-between'>
 <div>What's on your mind?</div>
-<div>
-<div className='w-[30px] h-[30px] bg-[#e2e2e7]'></div>
-<div className='w-[30px] h-[30px] bg-[#e2e2e7]'></div>
+<div className='flex'>
+<div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
+<div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
 
 
 </div>
