5dce77716e03ccc1c5f356a0dcb9b9a00504c862
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-30 04:59:33 +0530

    Fetching the Api

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { HiArrowSmRight, HiArrowSmLeft } from "react-icons/hi";
+
 
 export default function () {
+  const [categories, setCategory] = useState([]);
+
+const fetchCategory = async () =>{
+  const response = await fetch("http://localhost:5000/categories"); 
+  const data = await response.json();
+  setCategory(data);
+
+}
+useEffect(
+() => {
+fetchCategory();
+
+},  []
+
+
+)
+
   return (
-    <div className='max-w-[1200px] mx-auto flex items-center'>
+    <div className='max-w-[1200px] mx-auto'>
     <div className='flex items-center justify-between'>
-<div>What's on your mind?</div>
+<div className='text-[25px] font-bold'>What's on your mind?</div>
 <div className='flex'>
-<div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
-<div className='w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2'></div>
+<div className='cursor-pointer w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex justify-center items-center'> <HiArrowSmLeft/></div>
+<div className='cursor-pointer w-[30px] h-[30px] bg-[#e2e2e7] rounded-full mx-2 flex justify-center items-center'><HiArrowSmRight />
+</div>
 
 
 </div>
@@ -15,7 +35,18 @@ export default function () {
 
 
     </div>
-    
+    <div className='flex'>
+      {
+        categories.map(
+          (cat, index)=>{
+return(
+<div></div>
+
+)
+          }
+        )
+      }
+    </div>
     
     </div>
   )
