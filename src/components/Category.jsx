199244af12165689c7b39e85c82f9b5571423b5b
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-29 05:38:45 +0530

    minor upgrde

---
+++ src/components/Category.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function () {
+  return (
+    <div className='max-w-[1200px] mx-auto flex items-center'>
+    <div>
+<div>What's on your mind?</div>
+<div>
+<div className='w-[30px] h-[30px] bg-[#e2e2e7]'></div>
+<div className='w-[30px] h-[30px] bg-[#e2e2e7]'></div>
+
+
+</div>
+
+
+
+    </div>
+    
+    
+    </div>
+  )
+}
