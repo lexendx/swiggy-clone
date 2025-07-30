e7b036c75f0cf6febd0cecb02e54c03193973615
lexendx <tiwaribaibhav2004@gmail.com>
2025-07-31 05:03:36 +0530

    minor upgrade

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+import Category from "./components/Category";
+function App() {
+  return (
+    <>
+      <Header />
+      <Category/>
+    </>
+  );
+}
+
+export default App;
